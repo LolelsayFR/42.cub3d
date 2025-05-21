238441f14f700d854f78c4d501297581dbbcc438
emaillet <emaillet@student.42lehavre.fr>
2025-05-21 16:35:12 +0200

    Rename floor_texture and ceiling_texture to *_t

--- srcs/exec/raycasting/raycasting.c
+++ srcs/exec/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 19:40:14 by emaillet          #+#    #+#             */
-/*   Updated: 2025/05/21 16:08:10 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/05/21 16:34:41 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	raycasting(t_c3_data *data, t_pos pos, double angle)
 	(void)pos;
 	(void)angle;
 	mlx_put_image_to_window(data->mlx, data->win,
-		data->textures->floor_texture, 0, HEIGHT / 2);
+		data->textures->floor_t, 0, HEIGHT / 2);
 	mlx_put_image_to_window(data->mlx, data->win,
-		data->textures->ceiling_texture, 0, 0);
+		data->textures->ceiling_t, 0, 0);
 	return ;
 }
