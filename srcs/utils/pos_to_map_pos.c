01013fe6f05088798174fe692401d37e9c34dfb5
emaillet <emaillet@student.42lehavre.fr>
2025-05-21 15:57:21 +0200

    Update utils

--- srcs/utils/pos_to_map_pos.c
+++ srcs/utils/pos_to_map_pos.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   math_trigo.c                                       :+:      :+:    :+:   */
+/*   pos_to_map_pos.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/21 13:02:32 by emaillet          #+#    #+#             */
-/*   Updated: 2025/05/21 15:13:20 by emaillet         ###   ########.fr       */
+/*   Created: 2025/05/21 14:31:55 by emaillet          #+#    #+#             */
+/*   Updated: 2025/05/21 15:36:03 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.function.h"
 
-t_trigo	trigo(t_c3_data *data, double x, double y)
+t_pos	pos_to_map_pos(double x, double y)
 {
-	t_trigo	math;
+	t_pos	pos;
 
-	if (data->player->angle >= 360)
-		data->player->angle -= 360;
-	else if (data->player->angle < 0)
-		data->player->angle += 360;
-	math.angle_rad = data->player->angle * (N_PI / 180.0);
-	math.hypo = hypot(x, y);
-	math.opo = math.hypo * sin(math.angle_rad);
-	math.adj = math.hypo * cos(math.angle_rad);
-	return (math);
+	pos.y = (y / TILE_SIZE);
+	pos.x = (x / TILE_SIZE);
+	return (pos);
 }
