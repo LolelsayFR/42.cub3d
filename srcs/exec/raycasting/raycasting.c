d9c90ffad19537df3b12c9b75fc049b632ae872e
emaillet <emaillet@student.42lehavre.fr>
2025-05-21 15:56:12 +0200

    Better proto of raycasting

--- srcs/exec/raycasting/raycasting.c
+++ srcs/exec/raycasting/raycasting.c
@@ -6,8 +6,13 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 19:40:14 by emaillet          #+#    #+#             */
-/*   Updated: 2025/05/21 02:15:56 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/05/21 15:55:46 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.function.h"
+
+void	raycasting(t_c3_data *data, t_pos pos, double angle)
+{
+	return ;
+}
