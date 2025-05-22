183728d3cb20d116b7b67e18b3780f8c0024a301
Arthur Girard <artgirar@c1r1p1.42lehavre.fr>
2025-05-22 14:25:20 +0200

    prints modif

--- srcs/exec/raycasting/raycasting.c
+++ srcs/exec/raycasting/raycasting.c
@@ -6,23 +6,24 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 19:40:14 by emaillet          #+#    #+#             */
-/*   Updated: 2025/05/22 13:44:10 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/05/22 14:14:44 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.function.h"
 
-void	vertical_line(t_pos p_pos, double p_ang)
+void	vertical_line(t_c3_data *data, t_pos p_pos, double p_ang)
 {
 	double	ys;     
 	double	xs;
 	double	hypo;
 
-	(void)p_pos;
 	ys = 1;
 	xs = ys / p_ang;
 	hypo = hypot(xs, ys);
-	printf("%f\n", hypo);
+	printf("%f\n%f\n%f\n", xs, ys, hypo);
+	(void)data;
+	(void)p_pos;
 }
 
 void	horizontal_line(t_c3_data *data, t_pos p_pos, double p_ang)
@@ -34,7 +35,7 @@ void	horizontal_line(t_c3_data *data, t_pos p_pos, double p_ang)
 	xs = 1;
         ys = xs / p_ang;
         hypo = hypot(ys, xs);
-        printf("%f\n", hypo);
+        printf("%f\n%f\n%f\n", xs, ys, hypo);
 	(void)data;
 	(void)p_pos;
 }
@@ -46,6 +47,7 @@ void	raycasting(t_c3_data *data, t_pos pos, double angle)
 	mlx_put_image_to_window(data->mlx, data->win,
 		data->textures->ceiling_t, 0, 0);
 	horizontal_line(data, pos, angle);
-	vertical_line(pos, angle);
+	printf("\n");
+	vertical_line(data, pos, angle);
 	return ;
 }
