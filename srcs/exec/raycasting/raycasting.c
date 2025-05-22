5259beaac2bf98ea2f6cdb1d41dc094461b43aa1
Lolelsay <maillet.emilien@gmail.com>
2025-05-22 17:07:32 +0200

    Update raycasting.c

--- srcs/exec/raycasting/raycasting.c
+++ srcs/exec/raycasting/raycasting.c
@@ -42,10 +42,6 @@ void	horizontal_line(t_c3_data *data, t_pos p_pos, double p_ang)
 
 void	raycasting(t_c3_data *data, t_pos pos, double angle)
 {
-	mlx_put_image_to_window(data->mlx, data->win,
-		data->textures->floor_t, 0, HEIGHT / 2);
-	mlx_put_image_to_window(data->mlx, data->win,
-		data->textures->ceiling_t, 0, 0);
 	horizontal_line(data, pos, angle);
 	printf("\n");
 	vertical_line(data, pos, angle);
