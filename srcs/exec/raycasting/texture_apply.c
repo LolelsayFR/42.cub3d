d032b18978ffdd6233baf852d3421401fa182906
emaillet <emaillet@student.42lehavre.fr>
2025-05-27 09:33:04 +0200

    SegFix

--- srcs/exec/raycasting/texture_apply.c
+++ srcs/exec/raycasting/texture_apply.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 16:39:15 by emaillet          #+#    #+#             */
-/*   Updated: 2025/05/27 09:12:01 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/05/27 09:32:07 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,8 @@ void	texture_apply(t_img *img, int x, int y, t_ray ray)
 		return ;
 	if (ray.wally <= 0 || imgy <= 0)
 		return ;
-	pixel = ray.texture->data + ((imgy % ray.texture->height)
-			* ray.texture->size_line + (imgx % ray.texture->width)
-			* (ray.texture->bpp / 8));
-	color = darker_rgb(*(unsigned int *)pixel, ray.dist);
+	pixel = ray.texture->data + (imgy * ray.texture->size_line
+			+ (imgx % ray.texture->width) * (ray.texture->bpp / 8));
+	color = darker_rgb(*(unsigned long *)pixel, ray.dist);
 	img_pix_put(img, x, y, color);
 }
