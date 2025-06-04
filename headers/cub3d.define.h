36602a13a5c98f1ec342e26553f1e237b0947f2f
emaillet <emaillet@student.42lehavre.fr>
2025-06-05 00:28:05 +0200

    Some work in progress

--- headers/cub3d.define.h
+++ headers/cub3d.define.h
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 09:31:49 by emaillet          #+#    #+#             */
-/*   Updated: 2025/06/04 23:58:28 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/06/05 00:26:53 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@
 #  define RAY_DIVIDER	4
 #  define RAY_PRECISION	0.015
 #  define RENDER_DIST	30
-#  define DOOR_DIST		10
+#  define DOOR_DIST		5
 #  define MINIMAP_SIZE	200
 # endif
 # define TILE_SIZE		12
--- srcs/exec/door_using.c
+++ srcs/exec/door_using.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 09:05:52 by artgirar          #+#    #+#             */
-/*   Updated: 2025/06/04 23:57:55 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/06/05 00:25:33 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,9 @@ void	door_update(t_c3_data *d, int x, int y, t_door *doors)
 		doors->anim = 0;
 	if (doors->anim > 0.95)
 		doors->anim = 0.95;
-	if (doors->anim == 0
-		&& doors->open == false && doors->is_anim == false)
+	if (doors->anim <= 0
+		&& doors->open == false && doors->is_anim == false
+		&& d->map[(int)doors->pos.y][(int)doors->pos.x] == 'd')
 	{
 		d->map[(int)doors->pos.y][(int)doors->pos.x] = 'D';
 		create_minimap_img(d);
@@ -51,7 +52,7 @@ void	door_clock(t_c3_data *d, t_pos pos)
 		{
 			temp.x = (int)(pos.x + x);
 			temp.y = (int)(pos.y + y);
-			if (temp.x > 0 && temp.y > 0
+			if (temp.x >= 0 && temp.y >= 0
 				&& temp.x < d->map_size[1] && temp.y < d->map_size[0]
 				&& ft_strchr("dD", d->map[(int)temp.y][(int)temp.x]))
 				door_update(d, pos.x, pos.y, get_door_data(d, temp));
@@ -78,8 +79,8 @@ static void	door_toggle(t_c3_data *data, t_ray ray)
 		data->map[(int)ray.pos.y][(int)ray.pos.x] = 'd';
 		door->is_anim = true;
 		door->open = true;
+		create_minimap_img(data);
 	}
-	create_minimap_img(data);
 }
 
 void	door_using(t_c3_data *data)
@@ -92,16 +93,18 @@ void	door_using(t_c3_data *data)
 	pos = pos_to_map_pos(data->player->pos.x, data->player->pos.y);
 	ft_bzero(&ray, sizeof(t_ray));
 	ray.angle = data->player->angle * (N_PI / 180.0);
-	while (ft_strchr("Dd", map[(int)ray.pos.y][(int)ray.pos.x]) == NULL
-			&& ray.dist <= USE_RANGE)
+	ray.pos = pos;
+	ray.dist = 0;
+	while (ray.dist <= USE_RANGE)
 	{
 		raytrigo(&ray, ray.dist, pos);
+		if ((int)ray.pos.y < 0 || (int)ray.pos.x < 0
+			|| (int)ray.pos.y >= data->map_size[0]
+			|| (int)ray.pos.x >= data->map_size[1])
+			break ;
+		if (ft_strchr("Dd", map[(int)ray.pos.y][(int)ray.pos.x]))
+			break ;
 		ray.dist += RAY_PRECISION;
-		while ((int)pos.y == (int)ray.pos.y && (int)pos.x == (int)ray.pos.x)
-		{
-			raytrigo(&ray, ray.dist, pos);
-			ray.dist += RAY_PRECISION;
-		}
 	}
 	if ((int)pos.y == (int)ray.pos.y && (int)pos.x == (int)ray.pos.x)
 		return ;
